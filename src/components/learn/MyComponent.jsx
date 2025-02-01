1bb05a657e6d2e8af3568ab2055e1878608c6817
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-02-01 13:42:45 +0700

    js trong html

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,31 @@
+import './style.css';
+
 const MyComponent = () => {
+
+    // const sosnune = 'sos nu 1';
+    // const sosnune = 25;
+    // const sosnune = true;
+    // const sosnune = undefined;
+    // const sosnune = null;
+    // const sosnune = [1, 2, 3];
+    const sosnune = {
+        name: "sosnu ne",
+        age: "24"
+    }
+
+
+
     return (
-        <div>
-            sos nu ne
-        </div>
+
+        <>
+            <div> {JSON.stringify(sosnune)} sos nu ne
+            </div>
+            <div>
+                {console.log("SOS NU")}
+            </div>
+            <div className="child" style={{ borderRadius: "10px" }}>
+            </div>
+        </>
     );
 }
 
