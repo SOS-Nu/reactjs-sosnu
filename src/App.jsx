9b35cfd86d9ab6ffbf5f9afd1563d6cf255b6b89
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-02-03 15:29:11 +0700

    design layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 
-
 import "./components/todo/todo.css"
-
+import TodoData from "./components/todo/TodoData"
+import TodoNew from "./components/todo/TodoNew"
+import reactLogo from "./assets/react.svg"
 
 const App = () => {
 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
-      <div className="todo-new">
-        <input type="text" />
-        <button>Add</button>
-      </div>
-      <div className="todo-data">
-        <div> learning react</div>
-        <div> watching yotube</div>
-
+      <TodoNew />
+      <TodoData />
+      <div>
+        <div className="todo-image">
+          <img src={reactLogo} className="logo react" />
+        </div>
       </div>
     </div>
   )
---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+
+
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div> learning react</div>
+            <div> watching yotube</div>
+        </div>
+    )
+}
+
+export default TodoData;
