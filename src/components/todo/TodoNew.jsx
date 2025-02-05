cbae9d9cc3e2d86383a2d99bed380ab68dc7a441
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-02-05 19:08:42 +0700

    onclick , onchange handle arrow function

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,14 +1,26 @@
 
 
 const TodoNew = (props) => {
-    console.log("check props", props);
+    // console.log("check props", props);
 
     const { addNewTodo } = props;
     // addNewTodo("sosnu");
+
+    const handleClick = () => {
+        alert(`click new`)
+    }
+    const handleOnChange = (name) => {
+        console.log(">> handleOnChange", name)
+    }
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.name)}
+            />
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
+
         </div >
     )
 }
