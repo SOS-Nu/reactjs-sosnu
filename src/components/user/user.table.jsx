5de1ce036e1e48c829b3c6d69923a6ad7064be57
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-02-24 19:52:50 +0700

    delete user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,15 +1,17 @@
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import { Table } from 'antd';
+import { Popconfirm, Table, notification } from 'antd';
 import UpdateUserModal from './update.user.modal';
-import { useState } from "react";
+import { useState } from 'react';
 import ViewUserDetail from './view.user.detail';
-
+import { deleteUserAPI } from '../../services/api.service';
 
 const UserTable = (props) => {
-
     const { dataUsers, loadUser } = props;
+
     const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+
     const [dataUpdate, setDataUpdate] = useState(null);
+
     const [dataDetail, setDataDetail] = useState(null);
     const [isDetailOpen, setIsDetailOpen] = useState(false);
 
@@ -19,14 +21,15 @@ const UserTable = (props) => {
             dataIndex: '_id',
             render: (_, record) => {
                 return (
-
-                    <a href='#' onClick={() => {
-                        setDataDetail(record);
-                        setIsDetailOpen(true);
-                    }}>{record._id}</a>
+                    <a
+                        href='#'
+                        onClick={() => {
+                            setDataDetail(record);
+                            setIsDetailOpen(true);
+                        }}
+                    >{record._id}</a>
                 )
             }
-
         },
         {
             title: 'Full Name',
@@ -40,25 +43,53 @@ const UserTable = (props) => {
             title: 'Action',
             key: 'action',
             render: (_, record) => (
-                <div style={{ display: 'flex', gap: '20px' }}>
-                    <EditOutlined style={{ cursor: 'pointer', color: 'orange' }}
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined
                         onClick={() => {
-                            setIsModalUpdateOpen(true);
                             setDataUpdate(record);
+                            setIsModalUpdateOpen(true);
                         }}
-                    />
-                    <DeleteOutlined style={{ cursor: 'pointer', color: 'red' }} />
+                        style={{ cursor: "pointer", color: "orange" }} />
+                    <Popconfirm
+                        title="Xóa người dùng"
+                        description="Bạn chắc chắn xóa user này ?"
+                        onConfirm={() => handleDeleteUser(record._id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="left"
 
+                    >
+                        <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                    </Popconfirm>
                 </div>
-
             ),
         },
     ];
 
-    return (
+    const handleDeleteUser = async (id) => {
+        const res = await deleteUserAPI(id);
+        if (res.data) {
+            notification.success({
+                message: "Delete user",
+                description: "Xóa user thành công"
+            })
+            await loadUser();
 
+        } else {
+            notification.error({
+                message: "Error delete user",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
+
+    return (
         <>
-            <Table columns={columns} dataSource={dataUsers} rowKey={"_id"} />
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey={"_id"}
+            />
             <UpdateUserModal
                 isModalUpdateOpen={isModalUpdateOpen}
                 setIsModalUpdateOpen={setIsModalUpdateOpen}
@@ -66,18 +97,15 @@ const UserTable = (props) => {
                 setDataUpdate={setDataUpdate}
                 loadUser={loadUser}
             />
+
             <ViewUserDetail
                 dataDetail={dataDetail}
                 setDataDetail={setDataDetail}
                 isDetailOpen={isDetailOpen}
                 setIsDetailOpen={setIsDetailOpen}
-
             />
-
         </>
     )
-
-
 }
 
 export default UserTable;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -23,6 +23,11 @@ const updateUserAPI = (_id, fullName, phone) => {
     return axios.put(URL_BACKEND, data);
 }
 
+const deleteUserAPI = (id) => {
+    const URL_BACKEND = `/api/v1/user/${id}`;
+    return axios.delete(URL_BACKEND);
+}
+
 
 const fetchAllUserAPI = () => {
     const URL_BACKEND = "/api/v1/user";
@@ -32,5 +37,6 @@ const fetchAllUserAPI = () => {
 export {
     createUserAPI,
     updateUserAPI,
-    fetchAllUserAPI
+    fetchAllUserAPI,
+    deleteUserAPI
 }
