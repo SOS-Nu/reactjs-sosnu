1a08c3c22227adf6ff11b83f1f0a7e16bac14a24
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-03-04 10:02:36 +0700

    private route vs react

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import { Outlet } from "react-router-dom"
 import { getAccountAPI } from "./services/api.service"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "./components/context/auth.context"
+import { Spin } from "antd"
 
 
 const App = () => {
-  const { setUser } = useContext(AuthContext)
+  const { user, setUser, isAppLoading, setIsAppLoading } = useContext(AuthContext)
 
 
   useEffect(() => {
     fetchUserInfo()
   }
     , []);
+
 
 
   const fetchUserInfo = async () => {
@@ -24,6 +26,8 @@ const App = () => {
       setUser(res.data.user)
       console.log(res.data)
     }
+
+    setIsAppLoading(false);
   }
 
 
@@ -31,12 +35,29 @@ const App = () => {
   return (
 
     <>
-      <Header />
 
-      <Outlet />
-      <Footer />
+
+      {
+        isAppLoading === true ? <Spin
+          style={
+            {
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50, -50%)",
+            }
+          } />
+          :
+          <>
+            <Header />
+            <Outlet />
+            <Footer />
+          </>
+      }
 
     </>
+
+
   )
 }
 
